f2937e53125cfa51d46bbf07efe1eb6a2dbb0868
Lílian <lily.mosquitoes@gmail.com>
2023-06-07 11:34:26 +0300

    feat(login): add password field

--- src/pages/login.rs
+++ src/pages/login.rs
@@ -7,10 +7,16 @@ use yew::{
 #[function_component(Login)]
 pub(crate) fn login() -> Html {
     html! {
-        <input
-            id={"username_input_field"}
-            type={"text"}
-        />
+        <section id={"login"}>
+            <input
+                id={"username_input_field"}
+                type={"text"}
+            />
+            <input
+                id={"password_input_field"}
+                type={"password"}
+            />
+        </section>
     }
 }
 
@@ -36,29 +42,70 @@ mod test {
     }
 
     #[wasm_bindgen_test]
+    async fn username_input_field_exists() {
+        render_login().await;
+
+        let element = DOM::get_element_by_id("username_input_field");
+
+        assert!(element.is_some());
+    }
+
+    #[wasm_bindgen_test]
     async fn username_input_field_is_visible() {
         render_login().await;
 
-        let username_input_field =
-            DOM::get_element_by_id("username_input_field")
-                .expect("Element `username_input_field` to exist");
+        let element = DOM::get_element_by_id("username_input_field")
+            .expect("Element `username_input_field` to exist");
 
-        assert!(DOM::is_element_visible(&username_input_field));
+        assert!(DOM::is_element_visible(&element));
     }
 
     #[wasm_bindgen_test]
     async fn username_input_field_is_text_input() {
         render_login().await;
 
-        let username_input_field =
-            DOM::get_element_by_id("username_input_field")
-                .expect("Element `username_input_field` to exist");
-        let tag = username_input_field.tag_name();
-        let input_type = username_input_field.get_attribute("type");
+        let element = DOM::get_element_by_id("username_input_field")
+            .expect("Element `username_input_field` to exist");
+        let tag = element.tag_name();
+        let input_type = element.get_attribute("type");
 
         let is_text_input =
             &tag == "INPUT" && input_type == Some("text".to_string());
 
         assert!(is_text_input);
+    }
+
+    #[wasm_bindgen_test]
+    async fn password_input_field_exists() {
+        render_login().await;
+
+        let element = DOM::get_element_by_id("password_input_field");
+
+        assert!(element.is_some());
+    }
+
+    #[wasm_bindgen_test]
+    async fn password_input_field_is_visible() {
+        render_login().await;
+
+        let element = DOM::get_element_by_id("password_input_field")
+            .expect("Element `password_input_field` to exist");
+
+        assert!(DOM::is_element_visible(&element));
+    }
+
+    #[wasm_bindgen_test]
+    async fn password_input_field_is_text_input() {
+        render_login().await;
+
+        let element = DOM::get_element_by_id("password_input_field")
+            .expect("Element `password_input_field` to exist");
+        let tag = element.tag_name();
+        let input_type = element.get_attribute("type");
+
+        let is_password_input = &tag == "INPUT"
+            && input_type == Some("password".to_string());
+
+        assert!(is_password_input);
     }
 }
